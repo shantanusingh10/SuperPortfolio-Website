2d09c04a99ec6bb5bc6e5e364681db3ee65a75c2
Shantanu Singh <shantanusingh994@gmail.com>
2022-10-28 09:48:32 +0530

    add nav section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src= {ME} alt="me"/>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>100+ Worldwide</small>
+            </article>
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+ Completed Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti aspernatur fugiat minima 
+            cupiditate, totam inventore perspiciatis excepturi cum accusamus magni ad ea necessitatibus 
+            natus eius veniam dolorem non tempora quibusdam!
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+
+      </div>
+
+
+    </section>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import './header.css'
+import ME from '../../assets/me3.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <div className="container header__container">
+            <h5>Hello I am</h5>
+            <h1>Shantanu Singh</h1>
+            <h5 className="text-light">Full Stack Developer</h5>
+            <CTA/>
+            <HeaderSocials/>
+            <div className="me">
+                <img src= {ME} alt= "me"/>
+            </div>
+
+            <a href='#contact' className='scroll__down'>Scroll Down</a>
+        </div>
+    </header>
   )
 }
 
