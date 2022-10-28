3d4b48041700d1b54d9d02a2b5cbbff2b6080108
Shantanu Singh <shantanusingh994@gmail.com>
2022-10-29 00:35:25 +0530

    add exp,service section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,105 @@
 import React from 'react'
+import './services.css'
+import {BsCheck} from 'react-icons/bs'
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+
+      <div className="services__container">
+        <article className='service'>
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+          </ul>
+        </article>
+        {/* End of UI/UX */}
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+          </ul>
+        </article>
+        {/* End of Web Dev */}
+        <article className='service'>
+          <div className="service__head">
+            <h3>Back End Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+            <li>
+              <BsCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>  
+            </li>
+          </ul>
+        </article>
+        {/* End of  */}
+      </div>
+    </section>
   )
 }
 
