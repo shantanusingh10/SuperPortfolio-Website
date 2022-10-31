8c7384aa307ac7a6cf67868366568264ef682786
Shantanu Singh <shantanusingh994@gmail.com>
2022-10-31 13:07:40 +0530

    add footer + minor tweaks

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,10 +3,23 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsWhatsapp} from 'react-icons/bs'
-
-
+import { useRef } from 'react';
+import emailjs, { sendForm } from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7an9x2p', 'template_ramgudl', form.current, 'tTq_gvavO57zWZmtl')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
   return (
     <section id="contact">
       <h5>Get In Touch </h5>
@@ -15,26 +28,26 @@ const Contact = () => {
       <div className='container contact__container'>
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>shantanusingh.jobs@gmail.com</h5>
             <a href='mailto: shantanusingh.jobs@gmail.com' target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
-            <BsLinkedin/>
+            <BsLinkedin className='contact__option-icon'/>
             <h4>LinkedIn</h4>
             <h5>Shantanu Singh</h5>
             <a href='linkedlin.com' target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+91 8209848435</h5>
-            <a href='https://api.whatsapp.com/send?phone=+918209848435' target="_blank">Send a message</a>
+            <a href='https://api.whatsapp.com/send?phone=918209848435' target="_blank">Send a message</a>
           </article>
         </div>
         {/* End of contact options */}
-        <form action = ''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name = "name" placeholder= "Your Full Name" required />
           <input type="email" name = "email" placeholder= "Your Email" required />
           <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Shantanu</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'> Home </a></li>
+        <li><a href='about'> About </a></li>
+        <li><a href='experience'> Experience </a></li>
+        <li><a href='services'> Services </a></li>
+        <li><a href='portfolio'> Portfolio </a></li>
+        <li><a href='testimonials'> Testimonials </a></li>
+        <li><a href='contact'> Contact </a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href='linkdin'><BsLinkedin/></a>
+
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Shantanu Singh projects. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
